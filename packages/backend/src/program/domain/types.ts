542b1496919ad5829e5ca9c6e0ce67087231c42c
ttdm <thibaut.tezenas@gmail.com>
2024-03-21 10:15:28 +0100

    wip

--- packages/backend/src/program/domain/types.ts
+++ packages/backend/src/program/domain/types.ts
@@ -9,3 +9,10 @@ export interface QuestionnaireData {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any
 }
+
+
+interface QuestionnaireDataSpecificGoal {
+  
+}
+
+https://mission-transition-ecologique.beta.gouv.fr/api/programs?questionnaire_route=specific_goal&siret=83014132100034&codeNaf=62.01Z&codeNAF1=&ville=FONTENAY-SOUS-BOIS&codePostal=94120&région=Île-de-France&structure_sizes=TPE&denomination=MULTI&secteur=Programmation informatique&entreprise . code NAF niveau 1 . est A=non&entreprise . code NAF niveau 1 . est B=non&entreprise . code NAF niveau 1 . est C=non&entreprise . code NAF niveau 1 . est D=non&entreprise . code NAF niveau 1 . est E=non&entreprise . code NAF niveau 1 . est F=non&entreprise . code NAF niveau 1 . est G=non&entreprise . code NAF niveau 1 . est H=non&entreprise . code NAF niveau 1 . est I=non&entreprise . code NAF niveau 1 . est J=oui&entreprise . code NAF niveau 1 . est K=non&entreprise . code NAF niveau 1 . est L=non&entreprise . code NAF niveau 1 . est M=non&entreprise . code NAF niveau 1 . est N=non&entreprise . code NAF niveau 1 . est O=non&entreprise . code NAF niveau 1 . est P=non&entreprise . code NAF niveau 1 . est Q=non&entreprise . code NAF niveau 1 . est R=non&entreprise . code NAF niveau 1 . est S=non&entreprise . code NAF niveau 1 . est T=non&entreprise . code NAF niveau 1 . est U=non&entreprise . effectif=19&questionnaire . objectif prioritaire . est mon impact environnemental=non&questionnaire . objectif prioritaire . est ma performance énergétique=oui&questionnaire . objectif prioritaire . est diminuer ma consommation d'eau=non&questionnaire . objectif prioritaire . est rénover mon bâtiment=non&questionnaire . objectif prioritaire . est la mobilité durable=non&questionnaire . objectif prioritaire . est la gestion des déchets=non&questionnaire . objectif prioritaire . est l'écoconception=non&questionnaire . objectif prioritaire . est former ou recruter=non&questionnaire . objectif prioritaire . est faire des économies=non&questionnaire . objectif prioritaire . est investir durable=non&questionnaire . objectif prioritaire . est je ne sais pas encore=non
