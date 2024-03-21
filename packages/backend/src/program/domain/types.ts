1d8cab65bf778348f6c767ec082bf90c4d2445c9
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2024-03-21 10:38:34 +0100

    WIP

--- packages/backend/src/program/domain/types.ts
+++ packages/backend/src/program/domain/types.ts
@@ -10,9 +10,50 @@ export interface QuestionnaireData {
   [key: string]: any
 }
 
+type OuiNon = 'oui' | 'non'
 
 interface QuestionnaireDataSpecificGoal {
-  
+  siret: string
+  codeNaf: string
+  codeNAF1?: string
+  ville: string
+  codePostal: string
+  région: string
+  structure_sizes: string // enum ?
+  denomination: string
+  secteur: string
+  'entreprise . effectif': number
+  'questionnaire . objectif prioritaire . est mon impact environnemental': OuiNon
+  'questionnaire . objectif prioritaire . est ma performance énergétique': OuiNon
+  "questionnaire . objectif prioritaire . est diminuer ma consommation d'eau": OuiNon
+  'questionnaire . objectif prioritaire . est rénover mon bâtiment': OuiNon
+  'questionnaire . objectif prioritaire . est la mobilité durable': OuiNon
+  'questionnaire . objectif prioritaire . est la gestion des déchets': OuiNon
+  "questionnaire . objectif prioritaire . est l'écoconception": OuiNon
+  'questionnaire . objectif prioritaire . est former ou recruter': OuiNon
+  'questionnaire . objectif prioritaire . est faire des économies': OuiNon
+  'questionnaire . objectif prioritaire . est investir durable': OuiNon
+  'questionnaire . objectif prioritaire . est je ne sais pas encore': OuiNon
 }
 
-https://mission-transition-ecologique.beta.gouv.fr/api/programs?questionnaire_route=specific_goal&siret=83014132100034&codeNaf=62.01Z&codeNAF1=&ville=FONTENAY-SOUS-BOIS&codePostal=94120&région=Île-de-France&structure_sizes=TPE&denomination=MULTI&secteur=Programmation informatique&entreprise . code NAF niveau 1 . est A=non&entreprise . code NAF niveau 1 . est B=non&entreprise . code NAF niveau 1 . est C=non&entreprise . code NAF niveau 1 . est D=non&entreprise . code NAF niveau 1 . est E=non&entreprise . code NAF niveau 1 . est F=non&entreprise . code NAF niveau 1 . est G=non&entreprise . code NAF niveau 1 . est H=non&entreprise . code NAF niveau 1 . est I=non&entreprise . code NAF niveau 1 . est J=oui&entreprise . code NAF niveau 1 . est K=non&entreprise . code NAF niveau 1 . est L=non&entreprise . code NAF niveau 1 . est M=non&entreprise . code NAF niveau 1 . est N=non&entreprise . code NAF niveau 1 . est O=non&entreprise . code NAF niveau 1 . est P=non&entreprise . code NAF niveau 1 . est Q=non&entreprise . code NAF niveau 1 . est R=non&entreprise . code NAF niveau 1 . est S=non&entreprise . code NAF niveau 1 . est T=non&entreprise . code NAF niveau 1 . est U=non&entreprise . effectif=19&questionnaire . objectif prioritaire . est mon impact environnemental=non&questionnaire . objectif prioritaire . est ma performance énergétique=oui&questionnaire . objectif prioritaire . est diminuer ma consommation d'eau=non&questionnaire . objectif prioritaire . est rénover mon bâtiment=non&questionnaire . objectif prioritaire . est la mobilité durable=non&questionnaire . objectif prioritaire . est la gestion des déchets=non&questionnaire . objectif prioritaire . est l'écoconception=non&questionnaire . objectif prioritaire . est former ou recruter=non&questionnaire . objectif prioritaire . est faire des économies=non&questionnaire . objectif prioritaire . est investir durable=non&questionnaire . objectif prioritaire . est je ne sais pas encore=non
+// 'entreprise . code NAF niveau 1 . est A': OuiNon
+// 'entreprise . code NAF niveau 1 . est B': OuiNon
+// 'entreprise . code NAF niveau 1 . est C': OuiNon
+// 'entreprise . code NAF niveau 1 . est D': OuiNon
+// 'entreprise . code NAF niveau 1 . est E': OuiNon
+// 'entreprise . code NAF niveau 1 . est F': OuiNon
+// 'entreprise . code NAF niveau 1 . est G': OuiNon
+// 'entreprise . code NAF niveau 1 . est H': OuiNon
+// 'entreprise . code NAF niveau 1 . est I': OuiNon
+// 'entreprise . code NAF niveau 1 . est J': OuiNon
+// 'entreprise . code NAF niveau 1 . est K': OuiNon
+// 'entreprise . code NAF niveau 1 . est L': OuiNon
+// 'entreprise . code NAF niveau 1 . est M': OuiNon
+// 'entreprise . code NAF niveau 1 . est N': OuiNon
+// 'entreprise . code NAF niveau 1 . est O': OuiNon
+// 'entreprise . code NAF niveau 1 . est P': OuiNon
+// 'entreprise . code NAF niveau 1 . est Q': OuiNon
+// 'entreprise . code NAF niveau 1 . est R': OuiNon
+// 'entreprise . code NAF niveau 1 . est S': OuiNon
+// 'entreprise . code NAF niveau 1 . est T': OuiNon
+// 'entreprise . code NAF niveau 1 . est U': OuiNon
